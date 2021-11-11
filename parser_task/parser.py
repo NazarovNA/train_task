462644a9ce3d7649fd0cff4e00aaf167eb3c1176
NazarovNA <straider105@gmail.com>
2021-11-11 14:29:31 +0300

    Рефакторинг

--- parser_task/parser.py
+++ parser_task/parser.py
@@ -9,86 +9,34 @@ import logging
 
 class ParseExternalApi:
     """
-    Класс для загрузки и обновления данных справочников из внешнего API.
-    :param model: модель в которую необходимо обеспечить загрузку.
-    :param extend_extra_kwargs: словарь - дополнение к extra_kwargs. Ключами являеются наименования полей json для
-    занесения модели. Значениями являются словари, которые запоняются по шаблону
-    {'Поле json': {'foreign_model': 'Модель в которой ищем', 'foreign_model_lookup_field':'Поле по которому ищем',
-    'values_map': {'Какое значение': 'На какое заменяем'}}
-    Для указания поля pk в json с внешним кодом записи, по которому можно найти запись в модели необходимо заполнить
-    параметры foreign_model: и foreign_model_lookup_field.
-    Для замены определенных значений поля необходимо заполнить параметр values_map.
-    :param json_fields кортеж полей json для заполнения модели
-    :param extra_kwargs является таблией соответствий при различных наменованиях полей json и соответствующих
-    им полей модели. Передается в формате: {наименование поля json: 'source': наименование поля в модели}
-    :param base_url: ссылка на API, без указания параметров запроса.
-    :param url_settings: параметры запроса к API для url
+    Универсальный класс для импорта из API (REST full) в модель.
     """
 
-    def __init__(self, model, extend_extra_kwargs, json_fields, extra_kwargs, base_url, url_settings=''):
+    def __init__(self, model, extend_extra_kwargs, extra_kwargs, api_url, api_settings=''):
         """
-        :param model: модель в которую необходимо обеспечить загрузку.
-        :param extend_extra_kwargs: ссловарь - дополнение к extra_kwargs. Ключами являеются наименования полей json для
-        занесения модели. Значениями являются словари, которые запоняются по шаблону
-        {'Поле json': {'foreign_model': 'Модель в которой ищем', 'foreign_model_lookup_field':'Поле по которому ищем',
-        'values_map': {'Какое значение': 'На какое заменяем'}}
-        Для указания поля pk в json с внешним кодом записи, по которому можно найти запись в модели необходимо заполнить
-        параметры foreign_model: и foreign_model_lookup_field.
-        Для замены определенных значений поля необходимо заполнить параметр values_map.
-        :param json_fields кортеж полей json для заполнения модели
-        :param extra_kwargs является таблией соответствий при различных наменованиях полей json и соответствующих
-        им полей модели. Передается в формате: {наименование поля json: 'source': наименование поля в модели}
-        :param base_url: ссылка на API, без указания параметров запроса.
-        :param url_settings: параметры запроса к API для url
+        :param model: модель для загрузки
+        :param extend_extra_kwargs: расширенные опции для сериализатора ParseApiSerializer
+        :param extra_kwargs опции сериализатора
+        :param api_url: URL без указания параметров запроса
+        :param api_settings: параметры запроса к API
         """
 
         self.extend_extra_kwargs = extend_extra_kwargs
         self.extra_kwargs = extra_kwargs
-        self.json_fields = json_fields
         self.model = model
-        self.base_url = base_url
-        self.url_settings = url_settings
+        self.api_url = api_url
+        self.api_settings = api_settings
+
         self.logger = logging.getLogger(__name__)
-
-    def download_api(self):
-        """
-        Метод для получения набора данных из API и сохранения их в модель.
-        При вызове данного метода начинается загрузка и сохранение данных из внешней API.
-        """
-        page_number = 1
-        page_size = 1000
-        page_count = 1
-        not_imported_json_data = []
-        serializer = self.get_serializer()
-
-        # загрузка и запись/обновление данных
-        while page_number <= page_count:
-            request = self.make_request(page_size, page_number)  # произведение запроса к API
-            if request is None:
-                break
-            data = request['data']
-            page_count = int(request['pageCount'])
-            not_imported_json, imported = self.deserializing(serializer=serializer, data=data)
-            not_imported_json_data.extend(not_imported_json)
-            self.logger.info(f"Страница {page_number} из api загружена")
-            page_number += 1
-
-        # дозагрузка невалидных ранее данных
-        imported = 1
-        while not_imported_json_data and imported:
-            not_imported_json_data, imported = self.deserializing(serializer=serializer, data=not_imported_json_data)
-
-        if not not_imported_json_data:
-            self.deserializing(serializer=serializer, data=not_imported_json_data, flag_logger=True)
-            self.logger.warning(f"Загрузка в модель завершена. {len(not_imported_json_data)} записей невалидны.")
-        else:
-            self.logger.info('Загрузка в модель полностью завершена')
+        self.serializer = self.get_serializer()
 
     def make_request(self, size_page, page_number):
         """
-        Метод для отправки запроса к API.
+        Метод для отправки запроса к API и получения данных.
+        :param size_page: Размер страницы
+        :param page_number: Номер страницы
         """
-        url = f"{self.base_url}?pageSize={size_page}&{self.url_settings}&pageNum={page_number}"
+        url = f"{self.api_url}?pageSize={size_page}&{self.api_settings}&pageNum={page_number}"
         self.logger.info(f"url по которой происходит запрос: {url}")
         try:
             req = requests.get(url, timeout=10)
@@ -105,22 +53,23 @@ class ParseExternalApi:
             self.logger.error(f"Загрузка данных на странице {page_number} вызвала ошибку {req.status_code}")
             return None
 
-    def deserializing(self, serializer, data, flag_logger=False):
+    def deserializing(self, data, flag_logger=False):
         """
-        Метод, предназначенный для десериализации записи и сохранения ее в модель
-        :param serializer: настроенный сериализатор
-        :param data: данные, которые необходимо сохранить
-        :param flag_logger: флаг для включения логирования ошибок
+        Метод для десериализации записи и сохранения ее в модель
+        :param data: данные для десериализаии
+        :param flag_logger: флаг для включения логирования ошибок сохранения
         :return not_imported_json: данные, которые не удалось загрузить
-        :return imported: количество произведенных сохранений
+        :return imported: количество импортированых записей
         """
         not_imported_json = []
         imported = 0
         for record in data:
-            deserialized_data = serializer(data=copy.deepcopy(record))
+            deserialized_data = self.serializer(data=copy.deepcopy(record))
             if not deserialized_data.is_valid():
                 if flag_logger:
-                    self.logger.warning(deserialized_data.errors)
+                    self.logger.error(record, deserialized_data.errors)
+                else:
+                    self.logger.debug(record, deserialized_data.errors)
                 not_imported_json.append(record)
                 continue
             deserialized_data.save()
@@ -130,13 +79,45 @@ class ParseExternalApi:
 
     def get_serializer(self):
         """
-        Метод для создания сериализатора с настройками для указанной модели.
+        Метод для создания сериализатора.
         """
         class ConfiguredSerializer(ParseApiSerializer):
             class Meta:
                 model = self.model
-                fields = self.json_fields
+                fields = '__all__'
                 extra_kwargs = self.extra_kwargs
                 extend_extra_kwargs = self.extend_extra_kwargs
 
         return ConfiguredSerializer
+
+    def download_api(self):
+        """
+        Метод для получения данных из API и импорта их в модель.
+        """
+        page_number = 1
+        page_size = 1000
+        page_count = 1
+        not_imported_json = []
+
+        # загрузка и запись/обновление данных
+        while page_number <= page_count:
+            request = self.make_request(page_size, page_number)  # произведение запроса к API
+            if request is None:
+                break
+            data = request['data']
+            data.extend(not_imported_json)
+            page_count = int(request['pageCount'])
+            not_imported_json, imported = self.deserializing(data=data)
+            self.logger.info(f"Страница {page_number} из api загружена")
+            page_number += 1
+
+        # дозагрузка невалидных ранее данных
+        imported = 1
+        while not_imported_json and imported:
+            not_imported_json_data, imported = self.deserializing(data=not_imported_json)
+
+        if not not_imported_json:
+            self.deserializing(data=not_imported_json, flag_logger=True)
+            self.logger.warning(f"Загрузка в модель завершена. {len(not_imported_json)} записей невалидны.")
+        else:
+            self.logger.info('Загрузка в модель полностью завершена')
--- parser_task/serializers.py
+++ parser_task/serializers.py
@@ -5,25 +5,26 @@ from rest_framework import serializers
 
 class ParseApiSerializer(serializers.ModelSerializer):
     """
-    Сериализатор предназначенный для обеспечения загрузки и обновления справочников из API.
+    Потомок ModelSerializer, который для связей вместо первичного ключа позволяет использовать любые
+    другие поля связной модели, позволяющие определить уникальный элемент. Так же позволяет производить замену (меппинг)
+    значений полей по заданным соответствиям. Расширенные возможности реализованны только для десериализаии.
 
-    При работе с данным классом в Meta класс необходимо занести дополнительный параметр
-        extend_extra_kwargs: словарь - дополнение к extra_kwargs. Ключами являеются наименования полей json для
-    занесения модели. Значениями являются словари, которые запоняются по шаблону
-    {'Поле json': {'foreign_model': 'Модель в которой ищем', 'foreign_model_lookup_field':'Поле по которому ищем',
-    'values_map': {'Какое значение': 'На какое заменяем'}}
-    Для указания поля pk в json с внешним кодом записи, по которому можно найти запись в модели необходимо заполнить
-    параметры foreign_model: и foreign_model_lookup_field.
-    Для замены определенных значений поля необходимо заполнить параметр values_map.
+    Расширенные возможности описываются через опцию:
+    extend_extra_kwargs: использование аналогично extra_kwargs.
+    {'Поле json':
+        {
+        'foreign_model': 'Модель в которой ищем (на которую ссылаемся)',
+        'foreign_model_lookup_field':'Поле по которому ищем',
+        'values_map': {'Какое значение': 'На какое заменяем'}
+        }
+    }
     """
 
     def to_internal_value(self, data):
         """
-        Метод для поддержки десериализации для операций записи.
+        Переопределение метода родителя, реализующего особенности десериализации для данного класса.
         """
-        # получение параметров из Meta класса
         model = copy.deepcopy(getattr(self.Meta, 'model', None))
-
         extra_kwargs = self.Meta.extra_kwargs
         extend_extra_kwargs = self.Meta.extend_extra_kwargs
 
@@ -33,9 +34,9 @@ class ParseApiSerializer(serializers.ModelSerializer):
                 continue
             if json_field not in extend_extra_kwargs.keys():
                 continue
-            # Получение параметров поля
             value_extra_kwargs = extra_kwargs.get(json_field)
             value_extend_extra_kwargs = extend_extra_kwargs[json_field]
+
             # Меппинг значений поля
             try:
                 values_map = value_extend_extra_kwargs['values_map']
